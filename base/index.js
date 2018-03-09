5106833de497968ba312433a748c61bc04ad6822
fridolph <249121486@qq.com>
2018-03-09 20:32:59 +0800

    项目初始化准备就绪

--- base/index.js
+++ base/index.js
@@ -1,52 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
+// import 'antd-mobile/dist/antd-mobile.css'
+// import Clock from './components/Clock'
 import registerServiceWorker from './registerServiceWorker'
+
+// import App from './App'
 // redux demo
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+
 // router
 import { BrowserRouter, Route, Link, Redirect, Switch } from 'react-router-dom'
-//axios
-import axios from 'axios'
-import './config'
+// import Auth from './RouteDemo/Auth'
+import Dashboard from './RouteDemo/Dashboard'
 
-function reducer() {}
-
+// 合并reducers
+import {Counter} from './ReduxDemo/counter.redux'
+// import {Auth} from './RouteDemo/Autho.redux'
+// redux-devtools
 const store = createStore(
-  reducer,
+  // Auth,
   compose(
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   )
 )
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      data: []
-    }
-  }
-  render() {
-    const name = this.state.data.length > 0 ? this.state.data[0].name : ''
-    return (
-      <h2>hello, {name}</h2>
-    )
-  }  
-
-  componentDidMount() {
-    axios.get('/data').then(res => {
-      console.log('请求了')
-      // console.log(res.data)
-    })
-  }
-}
-
 ReactDOM.render(
   (<Provider store={store}>
     <BrowserRouter>
-      <App />
+      <div>
+        <Dashboard />
+      </div>
     </BrowserRouter>
   </Provider>),
   document.getElementById('root')
---
+++ src/config.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { Toast } from 'antd-mobile'
+
+// 拦截请求
+axios.interceptors.request.use(config => {
+  Toast.loading('加载中', 0)
+  return config
+})
+
+// 拦截响应
+axios.interceptors.response.use(config => {
+  setTimeout(() => {
+    Toast.hide()
+  }, 500)
+  return config
+})
